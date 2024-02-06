05981132832cc1ba3a4a52eeca9b8616dbec56c4
frabru99 <128138203+frabru99@users.noreply.github.com>
2024-02-06 16:22:29 +0100

    Completato Booth

---
+++ Elaborato ASDi/Reti Sequenziali/Esercizio6.2/MEM.vhd
@@ -0,0 +1,44 @@
+------MEMORIA MEM----------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity MEM is
+    port (
+        in_Mem : in STD_LOGIC_VECTOR(3 downto 0);
+        write_Mem : in STD_LOGIC;
+        adress_Mem : in STD_LOGIC_VECTOR(3 downto 0);
+        
+        
+        outMem : out  STD_LOGIC_VECTOR(3 downto 0)
+        
+    );
+end entity MEM;
+
+
+architecture rtl of MEM is
+
+    --Definizione della memoria come tipo
+    type mem_type is array (0 to 15) of STD_LOGIC_VECTOR(3 downto 0);
+    shared variable mem_v : mem_type := (others => x"0");
+    
+
+begin
+
+    processMem : process(write_Mem) is
+
+    begin
+        
+        
+        
+        if (falling_edge(write_Mem)) then
+            mem_v(to_integer(unsigned(adress_Mem))) := in_Mem;
+            outMem <= mem_v(to_integer(unsigned(adress_Mem)));
+        end if;
+
+    end process;
+    
+     
+    
+
+end architecture rtl;
---
+++ Elaborato ASDi/Reti Sequenziali/Esercizio6.2/contMod16.vhd
@@ -0,0 +1,68 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+
+entity contMod16 is
+    port (
+        clk : in std_logic;
+        reset : in std_logic;
+
+        cont: out std_logic_vector(3 downto 0) 
+    );
+end entity contMod16;
+
+
+architecture rtl of contMod16 is
+
+   component fftClassic
+        port(
+            clk : in std_logic;
+            reset : in std_logic; --reset dato dall'esterno
+            
+            Y : out std_logic
+        );
+    end component;
+        
+
+    signal wirings : std_logic_vector (3 downto 0);
+    signal reset_count : std_logic;
+    
+begin
+
+
+    ff0: fftClassic port map(
+        clk => clk,
+        reset => reset_count,
+
+        Y=> wirings(0)
+    );
+
+
+    gen : for i in 1 to 3 generate 
+
+        ffn: fftClassic port map(
+        clk => wirings(i-1),
+        reset => reset_count,
+
+        Y=> wirings(i)
+        );
+    end generate;
+
+    
+    cont <= wirings;
+     
+
+    p: process (wirings, clk, reset)
+    begin
+        reset_count<='0';
+        
+        if(reset='1') then
+            reset_count<='1';
+            
+        end if;
+        
+    end process;
+
+end architecture rtl;
